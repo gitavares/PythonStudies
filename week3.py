8e434e11641fb44c550682d0a9e3453750b8e8f9
Giselle Tavares <gitavares@hotmail.com>
2018-01-30 23:39:21 -0300

    exercise 4 concluded

--- week3.py
+++ week3.py
@@ -7,10 +7,27 @@ Created on Tue Jan 30 00:39:43 2018
 """
 
 def oddTuples(aTup):
-    newTuple = ('')
-    for x in range(0, len(aTup), 2):
-        newTuple += aTup[x]
+    newTuple = ()
+    for x in range(1, len(aTup)+1, 2):
+        newTuple += aTup[x-1:x]
     return newTuple
 
 print(oddTuples(('I', 'am', 'a', 'test', 'tuple')))
+
+
+
+#x = (1, 2, (3, 'John', 4), 'Hi')
+#x[0]
+#x[2]
+#x[-1]
+#x[2][2]
+#x[2][-1]
+#x[-1][-1]
+#x[-1][2]
+#x[0:1]
+#x[0:-1]
+#len(x)
+#2 in x
+#3 in x
+#x[0] = 8
         
