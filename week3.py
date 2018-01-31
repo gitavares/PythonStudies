59e1848c8847d2143fc97a98f766e3d1cfbabcde
Giselle Cristina Moreno Tavares - DATAPREVCE <giselle.tavares@dataprev.gov.br>
2018-01-31 08:01:53 -0300

    exercises Part 5 completed

--- week3.py
+++ week3.py
@@ -30,4 +30,24 @@ print(oddTuples(('I', 'am', 'a', 'test', 'tuple')))
 #2 in x
 #3 in x
 #x[0] = 8
-        
+
+
+def applyEachTo(L, x):
+    result = []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result   
+
+def square(a):
+    return a*a
+
+def halve(a):
+    return a/2
+
+def inc(a):
+    return a+1
+
+#print(applyEachTo([inc, square, halve, abs], -3))
+#print(applyEachTo([inc, square, halve, abs], 3.0))
+print(applyEachTo([inc, max, int], -3))
+#[-2, error, -3]
