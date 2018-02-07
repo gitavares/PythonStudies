b27cadf001392b9f65fa03a4083366dc24626b84
Giselle Cristina Moreno Tavares - DATAPREVCE <giselle.tavares@dataprev.gov.br>
2018-02-07 16:06:41 -0300

    working on Problem Set 4

---
+++ week4-exercise3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 13:33:26 2018
+
+@author: giselletavares
+"""
+
+#def normalize(numbers):
+#    max_number = max(numbers)
+#    for i in range(len(numbers)):
+#        numbers[i] /= float(max_number)
+#    return numbers
+#
+#try:
+#      normalize([0, 0, 0])
+#except ZeroDivisionError:
+#      print('Invalid maximum element')
+      
+      
+def normalize(numbers):
+    max_number = max(numbers)
+    assert(max_number != 0), "Cannot divide by 0"
+    for i in range(len(numbers)):
+        numbers[i]  /= float(max_number)
+        assert(0.0 <= numbers[i] <= 1.0), "output not between 0 and 1"
+    return numbers  
+
+try:
+      normalize([0, 0, 0])
+except ZeroDivisionError:
+      print('Invalid maximum element')
--- week4.py
+++ week4.py
@@ -33,3 +33,148 @@ def f(n):
       return n * f(n-1)
 
 print(f(0))
+
+#def fancy_divide(numbers,index):
+#    try:
+#        denom = numbers[index]
+#        print('denom=', denom)
+#        for i in range(len(numbers)):
+#            print('numbers[i]=',numbers[i])
+#            numbers[i] /= denom
+#        print('saiu do for')
+#    except IndexError:
+#        print('IndexError')
+#        print("-1")
+#    else:
+#        print('else')
+#        print("1")
+#    finally:
+#        print('finally')
+#        print("0")
+
+def fancy_divide(numbers,index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError:
+        print("-1")
+    else:
+        print("1")
+    finally:
+        print("0")
+
+print(fancy_divide([0, 2, 4], 0))
+
+print(fancy_divide([0, 2, 4], 4))
+
+print(fancy_divide([0, 2, 4], 1))
+#denom = 2
+#0,0,0
+
+
+def fancy_divide(numbers, index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError:
+        fancy_divide(numbers, len(numbers) - 1)
+    except ZeroDivisionError:
+        print("-2")
+    else:
+        print("1")
+    finally:
+        print("0")
+
+print(fancy_divide([0, 2, 4], 0))
+# -2, 0
+
+print(fancy_divide([0, 2, 4], 4))
+# denom = 4
+# 1, 0, 0
+
+print(fancy_divide([0, 2, 4], 1))
+# 1, 0
+
+
+def fancy_divide(numbers, index):
+    try:
+        try:
+            denom = numbers[index]
+            for i in range(len(numbers)):
+                numbers[i] /= denom
+        except IndexError:
+            fancy_divide(numbers, len(numbers) - 1)
+        else:
+            print("1")
+        finally:
+            print("0")
+    except ZeroDivisionError:
+        print("-2")
+        
+print(fancy_divide([0, 2, 4], 0))
+# 0, -2
+
+print(fancy_divide([0, 2, 4], 4))  
+# denom = 4
+# 1, 0, 0
+        
+print(fancy_divide([0, 2, 4], 1))
+# 1, 0
+
+def fancy_divide(list_of_numbers, index):
+    try:
+        try:
+            raise Exception("0")
+        finally:
+            denom = list_of_numbers[index]
+            for i in range(len(list_of_numbers)):
+                list_of_numbers[i] /= denom
+    except Exception as ex:
+        print(ex)
+        
+print(fancy_divide([0, 2, 4], 0))
+
+
+def fancy_divide(list_of_numbers, index):
+    try:
+        try:
+            denom = list_of_numbers[index]
+            for i in range(len(list_of_numbers)):
+                list_of_numbers[i] /= denom
+        finally:
+            raise Exception("0")
+    except Exception as ex:
+        print(ex)
+        
+print(fancy_divide([0, 2, 4], 0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
